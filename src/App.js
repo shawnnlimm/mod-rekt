83f918386e9ea44eb1964eb955ffc7bce2175ab1
shawn lim <shawnlim2525@gmail.com>
2023-05-19 16:34:49 +0800

    Added authentication page using Firebase and React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
-import React from 'react';
-import { Auth } from './components/auth';
+import "./App.css";
+import React from "react";
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDzrS61BWPaR9h9koylQzJSFiT0keZETqY",
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "mod-rekt.appspot.com",
   messagingSenderId: "982004405037",
   appId: "1:982004405037:web:0491106515065b9df5b9a4",
-  measurementId: "G-3K6T9KZCYQ"
+  measurementId: "G-3K6T9KZCYQ",
 };
 
 const app = initializeApp(firebaseConfig);
