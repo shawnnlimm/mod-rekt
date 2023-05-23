4d95c96096266c4bfa68f89c1e2b165e1410c901
shawn lim <shawnlim2525@gmail.com>
2023-05-23 16:51:40 +0800

    Added pages and stying with tailwind

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ toggle }) => {
+  return (
+    <nav
+      className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono"
+      role="navigation"
+    >
+      <Link to="/" className="pl-8">
+        ModRekt
+      </Link>
+      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={1.5}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+          />
+        </svg>
+      </div>
+      <div className="pr-8 md:block hidden">
+        <Link className="p-4" to="/">
+          Home
+        </Link>
+        <Link className="p-4" to="/menu">
+          Menu
+        </Link>
+        <Link className="p-4" to="/about">
+          About
+        </Link>
+        <Link className="p-4" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
