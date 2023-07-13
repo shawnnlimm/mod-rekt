383ad3503dd617953190e8c5009d0bac58e9ed8a
Zi Xuan <e0958049@u.nus.edu>
2023-07-14 01:06:37 +0800

    added background to pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
+  const backgroundImageUrl =
+  "https://drive.google.com/uc?export=view&id=10oRl6EW_LN3arqpPakfkUpY58Kk7ZhSD"; 
   return (
-    <div className="bg-white h-screen flex flex-col justify-center items-center">
-      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+    <div
+    className="h-screen flex flex-col justify-center items-center"
+    style={{
+      position: "relative",
+      background: "white",
+    }}
+  >
+    <div
+      className="absolute top-0 left-0 w-full h-full"
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundSize: "55%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        opacity: 0.25, 
+      }}
+    ></div>
+      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14 relative">
         ModRekt
       </h1>
       <Link
-        className="py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce"
+        className="py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce relative"
         to="/signup"
       >
         Sign Up Now
